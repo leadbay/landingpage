5f3021a7d94badcfad43efd23f806b3e5d529dd8
Tristan Remy <tristan.remy1@gmail.com>
2023-07-12 09:28:01 +0200

    try removing second form

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -54,35 +54,6 @@ export default function Footer() {
 							<p className="mt-2 leading-6 text-violet-200/70">
 								Monthly digest of what's new and exciting from us.
 							</p>
-							<form
-								className="mt-6 sm:flex sm:max-w-md"
-								name="newsletter"
-								action="/"
-								method="POST"
-								data-netlify="true"
-							>
-								<label htmlFor="newsletter-email-address" className="sr-only">
-									Email address
-								</label>
-								<input
-									type="email"
-									name="newsletter"
-									id="newsletter-email-address"
-									autoComplete="email"
-									required
-									className="w-full min-w-0 appearance-none rounded-md border-0 bg-white/5 px-6 py-4 text-base text-white ring-2 ring-inset  ring-white/10 duration-200 placeholder:text-violet-200 hover:bg-white/10 focus:ring-2 focus:ring-inset focus:ring-violet-500 sm:w-64 sm:sm:leading-6 xl:w-full"
-									placeholder="Email address"
-								/>
-								<div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
-									<button
-										type="submit"
-										className=" flex w-full transform items-center justify-center space-x-2 rounded-md bg-violet-200 px-6 py-4 font-semibold text-violet-900 transition duration-200 hover:translate-x-1 hover:bg-white focus-visible:outline  focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-500 motion-reduce:transition-none motion-reduce:hover:transform-none"
-									>
-										Subscribe
-										<ArrowSmallRightIcon className="ml-2 h-6 w-6 text-violet-900" />
-									</button>
-								</div>
-							</form>
 						</div>
 					</div>
 					<div className="border-t border-violet-200/10 p-8 md:flex md:items-center md:justify-between">
