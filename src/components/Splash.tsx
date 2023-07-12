02124cdec6cbc0a3879cf52f9c9918d20496bed6
Tristan Remy <tristan.remy1@gmail.com>
2023-07-12 09:11:55 +0200

    out comp

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,4 +1,5 @@
 import { Container, RequestAccess } from '@/components'
+import { ArrowSmallRightIcon } from '@heroicons/react/20/solid'
 
 export default function Splash() {
 	return (
@@ -15,7 +16,34 @@ export default function Splash() {
 						Lead supply and behavioral targeting for B2B sales.
 					</p>
 					<div className="mt-8 flex items-center justify-center gap-x-6">
-						<RequestAccess />
+						<form
+							className="mt-6 sm:flex sm:max-w-md"
+							name="waiting"
+							method="POST"
+							data-netlify="true"
+						>
+							<label htmlFor="email-address" className="sr-only">
+								Email address
+							</label>
+							<input
+								type="email"
+								name="email-address"
+								id="waiting-email-address"
+								autoComplete="email"
+								required
+								className="w-full min-w-0 appearance-none rounded-md border-0 bg-white/5 px-6 py-4 text-base text-white ring-2 ring-inset  ring-white/10 duration-200 placeholder:text-violet-200 hover:bg-white/10 focus:ring-2 focus:ring-inset focus:ring-violet-500 sm:w-64 sm:sm:leading-6 xl:w-full"
+								placeholder="Enter your email"
+							/>
+							<div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
+								<button
+									type="submit"
+									className=" flex w-full transform items-center justify-center space-x-2 rounded-md bg-violet-200 px-6 py-4 font-semibold text-violet-900 transition duration-200 hover:translate-x-1 hover:bg-white focus-visible:outline  focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-500 motion-reduce:transition-none motion-reduce:hover:transform-none"
+								>
+									Request access
+									<ArrowSmallRightIcon className="ml-2 h-6 w-6 text-violet-900" />
+								</button>
+							</div>
+						</form>
 					</div>
 				</div>
 				<div
