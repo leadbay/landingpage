4b23eb948a6a12e5b92b2a8bb52e050ffe258f94
Tristan Remy <tristan.remy1@gmail.com>
2023-10-02 10:18:12 +0200

    fix: alaways display form

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,11 +7,6 @@ import { motion } from "framer-motion"
 import { useState } from "react"
 
 const Hero = ({ className, ...props }: { className?: string }) => {
-  const [displayRequestForm, setdisplayRequestForm] = useState(false)
-
-  const handleRequestClick = () => {
-    setdisplayRequestForm(true)
-  }
   return (
     <Container
       className={cn(
@@ -51,38 +46,29 @@ const Hero = ({ className, ...props }: { className?: string }) => {
         <h2 className='mb-6 text-xl leading-loose sm:text-2xl'>
           AI-lead supply and behavioral targeting for B2B sales.
         </h2>
-        {displayRequestForm ? (
-          <form
-            className='flex justify-center gap-2'
-            name='waitinglist'
-            method='POST'
-            data-netlify='true'
-          >
-            <input type='hidden' name='form-name' value='Waitinglist' />
-            <input
-              type='email'
-              name='email-address'
-              id='waitinglist-email-address'
-              autoComplete='email'
-              required
-              placeholder='Enter your email...'
-              className='rounded-md border-2 border-gray-300 px-3 py-2 focus:outline-gray-500'
-            />
-            <button
-              type='submit'
-              className='rounded-md border-2 border-black bg-black px-6 py-2 text-white hover:border-gray-700 hover:bg-gray-700'
-            >
-              Submit
-            </button>
-          </form>
-        ) : (
+        <form
+          className='flex justify-center gap-2'
+          name='waitinglist'
+          method='POST'
+          data-netlify='true'
+        >
+          <input type='hidden' name='form-name' value='Waitinglist' />
+          <input
+            type='email'
+            name='email-address'
+            id='waitinglist-email-address'
+            autoComplete='email'
+            required
+            placeholder='Enter your email...'
+            className='rounded-md border-2 border-gray-300 px-3 py-2 focus:outline-gray-500'
+          />
           <button
-            onClick={handleRequestClick}
-            className='rounded-md border-2 border-black bg-black px-6 py-2 text-white hover:animate-wiggle hover:border-gray-700 hover:bg-gray-700'
+            type='submit'
+            className='rounded-md border-2 border-black bg-black px-6 py-2 text-white hover:border-gray-700 hover:bg-gray-700'
           >
             Request Access
           </button>
-        )}
+        </form>
       </motion.div>
     </Container>
   )
