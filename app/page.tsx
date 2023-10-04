4ba92f8bd1c41dd15a7bf70a35a9231e860e5636
Tristan Remy <tristan.remy1@gmail.com>
2023-10-04 13:11:38 +0200

    fix: record type

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,29 @@
 import { Hero } from "@/components"
 import ReactMarkdown from "react-markdown"
 
+interface RecordType {
+  id: string
+  fields: {
+    name: string
+    picture: [
+      {
+        url: string
+      }
+    ]
+    company_logo: [
+      {
+        thumbnails: {
+          small: {
+            url: string
+          }
+        }
+      }
+    ]
+    title: string
+    description: string
+  }
+}
+
 async function getChampions() {
   const res = await fetch(
     "https://api.airtable.com/v0/appXbiCXzlzJD4GFG/tblmWkzgrDt4yA6De",
@@ -22,13 +45,12 @@ async function getChampions() {
 
 export default async function Home() {
   const champions = await getChampions()
-  console.log(champions.records[2].fields?.picture?.[0]?.url)
 
   return (
     <>
       <Hero />
       <div className='flex w-full gap-2'>
-        {champions?.records?.map((record) => (
+        {champions?.records?.map((record: RecordType) => (
           <div key={record.id} className='p-6 bg-white text-black'>
             <h1>{record?.fields?.name}</h1>
             <img src={record?.fields?.picture?.[0]?.url} />
